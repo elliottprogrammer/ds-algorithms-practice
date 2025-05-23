94a0caf2cbfcbc10b18ca425d24ce5990e08b400
Bryan Elliott <bryan@elliottprogrammer.com>
2025-05-23 15:28:47 -0400

    Add memoization to recursive Fib solution.

--- fib/index.js
+++ fib/index.js
@@ -20,26 +20,53 @@
 //     return arr[n];
 // }
 
-// Recursive solution (My take.. It seems to work!!)
-function fib(n, arr = [0,1], i = 1) {
-    if (i >= n) {
-        return arr[n];
-    }
-    const num1 = arr[i];
-    const num2 = arr[i - 1];
-    arr.push(num1 + num2);
+// Recursive solution
+// function fib(n, arr = [0,1], i = 1) {
+//     if (i >= n) {
+//         return arr[n];
+//     }
+//     const num1 = arr[i];
+//     const num2 = arr[i - 1];
+//     arr.push(num1 + num2);
     
-    return fib(n, arr, i + 1);
+//     return fib(n, arr, i + 1);
 
+// }
+
+// // Recursive solution (memorize this!)
+// function fib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+    
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// Memoization (Remember/memorize this!)
+function memoize(fn) {
+    const cache = {};
+
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
 }
 
-// Recursive solution (memorize this!)
-function fib(n, arr = [0,1], i = 1) {
+// Recursive solution (with memoization)
+function fib(n) {
     if (n < 2) {
         return n;
     }
     
     return fib(n - 1) + fib(n - 2);
 }
+
+fib = memoize(fib);
 
 module.exports = fib;
