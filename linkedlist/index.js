edeab5fcd686ff050b13a961f3c4e51455add271
Bryan Elliott <bryan@elliottprogrammer.com>
2025-05-25 17:44:58 -0400

    Add LinkedList.

---
+++ linkedlist/index.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);;
+    }
+
+    size() {
+        let count = 0;
+        let node = this.head;
+
+        while(node) {
+            count += 1;
+            node = node.next;
+        }
+
+        return count;
+    }
+}
+
+module.exports = { Node, LinkedList };
